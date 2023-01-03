0e164bfb082aa6e77ed232f7a96c001b10523f5c
Stacy-Sanders <stacy.sanders081@gmail.com>
2023-01-03 17:23:31 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from question_model import Question
+from data import question_data
+
+question_bank = []
+for i in range(len(question_data)-1):
+    pair = Question(question_data[i]["text"], question_data[i]["answer"])
+    question_bank.append(pair)
+
+for i in range(len(question_bank)-1):
+    print(question_bank[i].text)
+    print(question_bank[i].answer)
+    print(20*"*")
+
+# print(question_bank[1].text)
+# print(question_bank[1].answer)
+
+print(question_bank)
