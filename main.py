5e63c0900a9bd2ea8de9750839cc1087fa4f3dcc
Stacy-Sanders <stacy.sanders081@gmail.com>
2023-01-04 13:55:06 -0500

    adding still has questions and check answer methods to quiz brain

--- main.py
+++ main.py
@@ -3,18 +3,21 @@ from data import question_data
 from quiz_brain import QuizBrain
 
 question_bank = []
-for i in range(len(question_data)-1):
+for i in range(len(question_data)):
     pair = Question(question_data[i]["text"], question_data[i]["answer"])
     question_bank.append(pair)
 
-for i in range(len(question_bank)-1):
-    print(question_bank[i].text)
-    print(question_bank[i].answer)
-    print(20*"*")
+# Testing
+# for i in range(len(question_bank)-1):
+#     print(question_bank[i].text)
+#     print(question_bank[i].answer)
+#     print(20*"*")
 
 # print(question_bank[1].text)
 # print(question_bank[1].answer)
+# print(question_bank)
 
-print(question_bank)
+quiz = QuizBrain(question_bank)
 
-QuizBrain(question_bank)
+while quiz.still_has_questions():  # if quiz still has questions remaining
+    quiz.next_question()
